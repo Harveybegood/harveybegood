fc13c053fd9284bad0128f87a32462a97de6890f
harveybegood <7615303+Harveybegood@users.noreply.github.com>
2021-09-10 16:26:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-MD_HEAD = """**<p align = "center">[Harvey's blog](https://harveybegood.github.io)</p>**
-**<p align = "center">about my tech learning and life</p>**
-## contact 
-- Blog: https://harveybegood.github.io
-"""
-
 # -*- coding: utf-8 -*-
 import argparse
 import os
