a20cb0c6acdfa7f1dd8a9800f1ce7d1117c6a41f
harveybegood <7615303+Harveybegood@users.noreply.github.com>
2021-09-10 18:04:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import os
 from github import Github
 
 MD_HEAD = """## Gitblog
-**<p align = "center">[Harvey's blog](https://harveybegood.github.io/myblog)</p>**
+**<p align = "center">[Harvey's blog](https://harveybegood.github.io/)</p>**
 **<p align = "center">about my tech learning and life</p>**
 ## contact 
-- Blog: https://harveybegood.github.io/myblog
+- Blog: https://harveybegood.github.io/
 """
 
 BACKUP_DIR = "backup"
