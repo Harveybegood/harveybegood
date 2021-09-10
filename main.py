0e644fec1ce6b88c47f2e766ed1cd6c4c449dcd4
harveybegood <7615303+Harveybegood@users.noreply.github.com>
2021-09-10 14:00:42 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+MD_HEAD = """**<p align = "center">[Harvey's blog](https://harveybegood.github.io)</p>**
+**<p align = "center">about my tech learning and life</p>**
+## contact 
+- Blog: https://harveybegood.github.io
+"""
+
